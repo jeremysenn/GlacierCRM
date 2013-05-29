15bb6b1d8b0969c1c0c5180b9abe832651ef2010
Jeremy Senn <jeremysenn@144net-29.sou.edu>
2013-05-29 13:28:52 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GlacierCRM::Application.config.secret_token = '368bc25dfb2df846faa3416e9df67d76f3497e9c10e327b4e43c28828d69df4ac4056fd61bfb3e914d31f8278dc0dcbc084dc17a314fb89a2276631c658717f5'
